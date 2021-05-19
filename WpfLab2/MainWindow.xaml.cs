fa19df63f73df7ba1ca6336b817c6389ad2fc06c
KStrzechowski <57865141+KStrzechowski@users.noreply.github.com>
2021-05-19 02:43:48 +0200

    Changing random figures initialization.

--- WpfLab2/MainWindow.xaml.cs
+++ WpfLab2/MainWindow.xaml.cs
@@ -18,95 +18,80 @@ namespace WpfLab2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            RandomFigures();
         }
 
-        private void RandomFigures()
+        private void mainCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            RandomFigures((Canvas)sender);
+        }
+        
+        private void RandomFigures(Canvas canvas)
         {
             var rand = new Random();
-            RectangleFunc1(rand);
-            RectangleFunc2(rand);
-            ElipseFunc1(rand);
-            ElipseFunc2(rand);
+
+            for (int i = 0; i < 4; i++)
+            {
+                var width = rand.Next(10, 400);
+                var height = rand.Next(10, 400);
+                var color = new SolidColorBrush(Color.FromRgb((byte)rand.Next(255), (byte)rand.Next(255), (byte)rand.Next(255)));
+
+                if (rand.Next() % 2 == 0)
+                {
+                    Rectangle figure = new Rectangle
+                    {
+                        Width = width,
+                        Height = height,
+                        Fill = color,
+                        Cursor = Cursors.Hand,
+                    };
+
+                    figure.MouseDown += figure_MouseDown;
+                    figure.GotFocus += figure_GotFocus;
+                    Canvas.SetLeft(figure, rand.Next((int)this.Width - width));
+                    Canvas.SetTop(figure, rand.Next((int)this.Height - height));
+                    canvas.Children.Add(figure);
+                }
+                else
+                {
+                    Ellipse figure = new Ellipse
+                    {
+                        Width = width,
+                        Height = height,
+                        Fill = color,
+                        Cursor = Cursors.Hand,
+                    };
+
+                    figure.MouseDown += figure_MouseDown;
+                    figure.GotFocus += figure_GotFocus;
+                    figure.Focusable = true;
+                    Canvas.SetLeft(figure, rand.Next((int)this.Width - width));
+                    Canvas.SetTop(figure, rand.Next((int)this.Height - height));
+                    canvas.Children.Add(figure);
+                }
+            }
         }
 
-        private void RectangleFunc1(Random rand)
+        private void figure_GotFocus(object sender, RoutedEventArgs e)
         {
-            Rectangle1.Width = rand.Next(100, 400);
-            Rectangle1.Height = rand.Next(100, 400);
-            Canvas.SetLeft(Rectangle1, rand.Next((int)this.Width));
-            Canvas.SetTop(Rectangle1, rand.Next((int)this.Height));
-        }
-        private void RectangleFunc2(Random rand)
-        {
-            Rectangle2.Width = rand.Next(100, 400);
-            Rectangle2.Height = rand.Next(100, 400);
-            Canvas.SetLeft(Rectangle2, rand.Next((int)this.Width));
-            Canvas.SetTop(Rectangle2, rand.Next((int)this.Height));
-        }
-        private void ElipseFunc1(Random rand)
-        {
-            Ellipse1.Width = rand.Next(100, 400);
-            Ellipse1.Height = rand.Next(100, 400);
-            Canvas.SetLeft(Ellipse1, rand.Next((int)this.Width));
-            Canvas.SetTop(Ellipse1, rand.Next((int)this.Height));
-        }
-        private void ElipseFunc2(Random rand)
-        {
-            Ellipse2.Width = rand.Next(100, 400);
-            Ellipse2.Height = rand.Next(100, 400);
-            Canvas.SetLeft(Ellipse2, rand.Next((int)this.Width));
-            Canvas.SetTop(Ellipse2, rand.Next((int)this.Height));
+            Ellipse figure = new Ellipse
+            {
+                Width = 100,
+                Height = 100,
+                Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                Cursor = Cursors.Hand,
+            };
+            mainCanvas.Children.Add(figure);
         }
 
-        private void Rectangle1_MouseDown(object sender, MouseButtonEventArgs e)
+        private void figure_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Rectangle1.Stroke = new SolidColorBrush(Color.FromRgb(255,255,255));
-            Rectangle1.StrokeThickness = 10;
-        }
 
-        private void Rectangle2_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Rectangle2.Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            Rectangle2.StrokeThickness = 10;
         }
-
-        private void Ellipse1_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Ellipse1.Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            Ellipse1.StrokeThickness = 10;
-        }
-
-        private void Ellipse2_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Ellipse2.Stroke = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-            Ellipse2.StrokeThickness = 10;
-        }
-
-        private void Rectangle1_MouseUp(object sender, MouseEventArgs e)
-        {
-            Rectangle1.StrokeThickness = 0;
-        }
-
-        private void Rectangle2_MouseUp(object sender, MouseEventArgs e)
-        {
-            Rectangle2.StrokeThickness = 0;
-        }
-
-        private void Ellipse1_MouseUp(object sender, MouseEventArgs e)
-        {
-            Ellipse1.StrokeThickness = 0;
-        }
-
-        private void Ellipse2_MouseUp(object sender, MouseEventArgs e)
-        {
-            Ellipse2.StrokeThickness = 0;
-        }
-
     }
 }
